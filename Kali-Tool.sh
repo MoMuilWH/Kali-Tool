9220047675af7b42dc9334552f3ca18dd6a3917b
MoMuilWH <43278110+MoMuilWH@users.noreply.github.com>
2018-09-16 18:44:33 +0300

    Tool Linux on Termux
    gg

---
+++ Kali-Tool.sh
@@ -0,0 +1,54 @@
+a="\033[1;30m"
+b="\033[1;31m"
+c="\033[1;32m"
+d="\033[1;33m"
+e="\033[1;34m"
+   
+
+
+
+
+                                 figlet "MoMuiLWH"
+              
+
+
+
+
+                    echo "            \033[1;34m               Welcome To Kali-Tool 💚"
+
+
+
+
+echo "                                                  1-Sqlmap"
+echo "                                                   2-beef"
+echo "                                                  3-TheFatRat"
+echo "                                                 4-A-Rat"
+echo "                                                5-metaSploit"
+echo "                                               6-xerosploit"
+echo "                                              7-routersploir"
+echo "                                              8-hulk"
+echo "                                              9-nmap"
+echo "                                              10-backdoor.apk"
+
+
+
+
+
+
+git clone https://github.com/sqlmapproject/sqlmap
+git clone https://github.com/beefproject/beef
+git clone https://github.com/Screetsec/TheFatRat
+git clone https://github.com/Xi4u7/A-Rat
+git clone https://github.com/rapid7/metasploit-framework
+git clone https://github.com/LionSec/xerosploit
+git clone https://github.com/threat9/routersploit
+git clone https://github.com/grafov/hulk
+git clone https://github.com/nmap/nmap
+git clone https://github.com/dana-at-cp/backdoor-apk
+
+
+
+                      echo "End Installed all tool Start Hacking 👿"
+
+
+            figlet "MWH"
